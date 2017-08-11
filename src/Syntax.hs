8737f9c6c68cda415967dea25cf50320d656742e
Sandro Lovnički <lovnicki.sandro@gmail.com>
2017-08-11 20:34:07 +0200

    src/Syntax.hs: [add] custom Show for Term

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -5,6 +5,11 @@ type Name = String
 data Term = Variable Name
           | Lambda Name Term
           | Application Term Term
-          deriving (Eq, Show)
+          deriving (Eq)
+
+instance Show Term where
+    show (Variable v)        = show v
+    show (Lambda n t)        = "λ" ++ (show n) ++ ".[" ++ (show t) ++ "]"
+    show (Application t1 t2) = "(" ++ (show t1) ++ " " ++ (show t2) ++ ")"
 
 
