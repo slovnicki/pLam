8deb0eb6b65f982503531133b95f04b2067ca746
Sandro Lovnički <lovnicki.sandro@gmail.com>
2017-08-13 16:29:21 +0200

    src/Syntax.hs:
    	- [update] lambda expression data names
    	- [add] assign, command, environment, error data

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,15 +1,36 @@
 module Syntax where
 
-type Name = String
+import Control.Monad.State
+import Text.Parsec hiding (State)
 
-data Term = Variable Name
-          | Lambda Name Term
-          | Application Term Term
-          deriving (Eq)
+type Variable = String
 
-instance Show Term where
-    show (Variable v)        = show v
-    show (Lambda n t)        = "λ" ++ (show n) ++ ".[" ++ (show t) ++ "]"
-    show (Application t1 t2) = "(" ++ (show t1) ++ " " ++ (show t2) ++ ")"
+data Expression = Variable Variable
+                | Abstraction Variable Expression
+                | Application Expression Expression
+                deriving (Eq)
+
+instance Show Expression where
+    show (Variable v)        = v
+    show (Abstraction n t)   = "λ" ++ n ++ "." ++ show t
+    show (Application t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
+
+data Command = Assign Variable Expression
+             deriving (Eq, Show)
+
+data LCILine = Command Command
+             | Expression Expression
+             deriving (Eq, Show)
+
+type Environment = [(Variable, Expression)]
+
+type Program = State Environment
+
+data Error = SyntaxError ParseError
+           | UndeclaredVariable Variable
+           | FatalError String
+           deriving (Show)
+
+type Failable = Either Error
 
 
