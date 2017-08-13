797d088d791da77642ffb393b49de3cdf386d52e
Sandro Lovnički <lovnicki.sandro@gmail.com>
2017-08-13 16:41:35 +0200

    src/Parser.hs: [rename, update], [add] assignment parsing

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,42 +1,71 @@
-module Parser (parseExpr) where
+module Parser where
 
-import Data.Char
-import Text.Parsec
-import Text.Parsec.String (Parser)
-import Text.ParserCombinators.Parsec.Language (emptyDef)
-
-import qualified Text.ParserCombinators.Parsec.Token as Token
+import Control.Monad.State
+import Text.Parsec hiding (State)
 
 import Syntax
 
 
-lexer :: Token.TokenParser ()
-lexer = Token.makeTokenParser emptyDef
+type Parser = Parsec String ()
 
-variable :: Parser Term
-variable = do
-  x <- Token.identifier lexer
-  return (Variable x)
+symbol :: Parser Char
+symbol = oneOf "`~!@#$%^&*-_+|;:',/?[]<>"
 
-lambda :: Parser Term
-lambda = do
-  (Token.reservedOp lexer) "\\"
-  args <- many1 (Token.identifier lexer)
-  (Token.reservedOp lexer) "."
-  body <- expr
-  return $ foldr Lambda body args
+identifier :: Parser String
+identifier = many1 $ letter <|> symbol <|> digit
 
-term :: Parser Term
-term =  (Token.parens lexer) expr
-    <|> variable
-    <|> lambda
+parseVariable :: Parser Expression
+parseVariable = liftM Variable $ identifier
 
-expr :: Parser Term
-expr = do
-  es <- many1 term
-  return (foldl1 Application es)
+parseAbstraction :: Parser Expression
+parseAbstraction = do
+  char '\\'
+  xs <- identifier `endBy1` spaces
+  char '.'
+  spaces
+  body <- parseApplication
+  return $ curry xs body where
+        curry (x:xs) body = Abstraction x $ curry xs body
+        curry [] body     = body
 
-parseExpr :: String -> Either ParseError Term
-parseExpr input = parse (expr) "<stdin>" input
+parseApplication :: Parser Expression
+parseApplication = do
+  es <- parseExpression `sepBy1` spaces
+  return $ foldl1 Application es
 
+parseParens :: Parser Expression
+parseParens = between (char '(') (char ')') parseApplication
+
+parseExpression :: Parser Expression
+parseExpression = parseVariable 
+               <|> parseAbstraction
+               <|> parseParens
+
+parseAssign :: Parser Command
+parseAssign = do
+    f <- identifier
+    spaces
+    xs <- identifier `endBy` spaces
+    char '='
+    spaces
+    y <- parseAbstraction
+    return $ Assign f $ curry xs y where
+        curry (x:xs) y = Abstraction x $ curry xs y
+        curry [] y     = y
+
+
+{-
+readCommand :: String -> Failable Command
+readCommand input = case parse parseAssign "<stdin>" input of
+  Left err -> Left $ SyntaxError err
+  Right ex -> Right ex
+-}
+
+readLambda :: String -> Failable Expression
+readLambda input = case parse parseExpression "<stdin>" input of
+  Left err -> Left $ SyntaxError err
+  Right ex -> Right ex
+
+readExpr :: String -> Failable Expression
+readExpr input = readLambda input
 
