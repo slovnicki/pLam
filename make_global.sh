72e344f84f36d1aa6b79cdbf7b082c58581112b1
Sandro Lovnički <lovnicki.sandro@gmail.com>
2018-11-18 18:32:44 +0100

    [add] make_global script for creating a global 'plam' command

---
+++ make_global.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+################################################
+# make the 'plam' command global in the system #
+################################################
+
+path_to_project=$(pwd)
+echo "Your path to this project is $path_to_project"
+path_to_bin=$(find .stack-work/install -name plam)
+echo "Your path to plam binary is $path_to_bin"
+
+plam_path="$path_to_project/$path_to_bin"
+
+echo "... creating a script 'plam' in /usr/local/bin"
+echo "#!/bin/bash" > /usr/local/bin/plam
+echo $plam_path >> /usr/local/bin/plam
+
+echo "... making it executable"
+chmod +x /usr/local/bin/plam
+
+echo "Success!"
+
+
